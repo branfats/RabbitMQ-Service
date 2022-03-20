470ddc71b672be8710397c434cdd477439cd2c6f
branfats <92558018+branfats@users.noreply.github.com>
2022-03-20 14:18:56 +0800

    For reference

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+const sendGridMail = require('@sendgrid/mail')
+const API_KEY = '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'
 
+sendGridMail.setApiKey(API_KEY)
+
+function createEmail(to, subject, text){
+    return{
+        to,
+        from: {
+            name :'Ecommerce Shop',
+            email: 'diyanahj.2020@smu.edu.sg'
+        },
+        subject,
+        text,
+        html: `<p>${text}</p>`
+    };
+}
+
+function sendEmail(to, subject, text){
+    sendGridMail
+    .send(createEmail(to,subject,text))
+    .then((response) => console.log("Email sent..."))
+    .catch((error) => console.log(error.message));
+}
+
+//sample call that works 
+//sendEmail('diyanahjamal@gmail.com', 'test', 'test');
