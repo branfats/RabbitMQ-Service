ec0afcbd9cbf7423cb955b318c2e6c9eef961667
branfats <92558018+branfats@users.noreply.github.com>
2022-03-20 14:02:33 +0800

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -11,6 +11,7 @@ def pdf_process_function(msg):
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
 url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
 params = pika.URLParameters(url)
+params.socket_timeout = 5
 connection = pika.BlockingConnection(params)
 channel = connection.channel() # start a channel
 channel.queue_declare(queue='pdfprocess') # Declare a queue
