d988e343477b663679d44256d5d00c561d62d246
branfats <92558018+branfats@users.noreply.github.com>
2022-03-21 14:15:40 +0800

    Update send.js

--- send.js
+++ send.js
@@ -1,30 +1,32 @@
 // Sample template from https://www.rabbitmq.com/tutorials/tutorial-one-javascript.html
+// This tutorial assumes RabbitMQ is installed and running on localhost on the standard port (5672). In case you use a different host, port or credentials, connections settings would require adjusting.
+// require amqp.node client libary to be installed
 
 #!/usr/bin/env node
 
 var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function(error1, channel) {
-    if (error1) {
-      throw error1;
+    if (error0) {
+        throw error0;
     }
-    var queue = 'hello';
-    var msg = 'Hello world';
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
 
-    channel.assertQueue(queue, {
-      durable: false
+        var queue = 'hello';
+        var msg = 'Hello World!';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log(" [x] Sent %s", msg);
     });
-
-    channel.sendToQueue(queue, Buffer.from(msg));
-    console.log(" [x] Sent %s", msg);
-  });
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
 });
-
-setTimeout(function() {
-  connection.close();
-  process.exit(0)
-  }, 500);
