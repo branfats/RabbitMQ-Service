6f55f1a65a35c54b41c09bf65b8f3c4a562cca1e
branfats <92558018+branfats@users.noreply.github.com>
2022-03-22 13:08:33 +0800

    Update send.js

--- send.js
+++ send.js
@@ -16,7 +16,18 @@ amqp.connect('amqp://localhost', function(error0, connection) {
         }
 
         var queue = 'checkout';
-        var msg = 'Hello World!';
+        
+        var msg = 'THANK YOU FOR YOUR ORDER!
+Hey ______,
+
+This email contains the status of your recent order #202560593. At this time, we are starting to work on your order and are unable to cancel items or make changes.
+ORDERS TYPICALLY SHIP WITHIN 1-3 BUSINESS DAYS
+Once your order ships you will receive an email confirmation with your tracking information. It can take up to 24 hours for your tracking information to be updated by the carrier. Thank you for your patience and understanding.
+Please note some items may ship separately due to availability, including any pre-order items.
+
+Have a great day ahead!
+
+ESD eCommerce Team';
 
         channel.assertQueue(queue, {
             durable: true
